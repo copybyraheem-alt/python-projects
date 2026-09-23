878c2369f06fba334ee844bb999a7fa3331c6cd6
copyb <copybyraheem@gmail.com>
2026-09-23 17:45:53 +0530

    Add Topic 59: File Detection

---
+++ File Detection.py
@@ -0,0 +1,16 @@
+import os
+
+
+def check_path(path):
+    if os.path.exists(path):
+        if os.path.isfile(path):
+            print(f"File found: {os.path.getsize(path)}")
+        elif os.path.isdir(path):
+            print(f"Folder found: -{os.path.getsize(path)} bytes")
+    else:
+        print("Path not found")
+
+
+
+check_path(".")
+check_path("banking.py")
